8883087dbf1f324a6318f1f7c6391ae70b2b8db5
sinzii <thang@ipvideomarket.info>
2022-05-26 12:51:07 +0700

    Update weighting

--- pallets/messaging/src/lib.rs
+++ pallets/messaging/src/lib.rs
@@ -108,8 +108,7 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		// #[pallet::weight(100_000 + T::DbWeight::get().reads_writes(5,7))]
-		#[pallet::weight(100_000)]
+		#[pallet::weight(100_000 + T::DbWeight::get().reads_writes(4,10))]
 		pub fn new_message(
 			origin: OriginFor<T>,
 			dest: <T::Lookup as StaticLookup>::Source,
